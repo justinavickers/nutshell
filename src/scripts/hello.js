95145e9f7b0854d2cefb5bec92756e49537ca42d
justinavickers <justinav38@gmail.com>
2018-11-12 11:18:24 -0600

    added jquery

--- src/scripts/hello.js
+++ src/scripts/hello.js
@@ -1,1 +1,1 @@
-export default () => console.warn("Hello!");
+
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,1 @@
-import sayHello from "./hello"
-import sayGoodbye from "./goodbye"
-import SandwichMaker from "./sandwichMaker"
 
-sayHello()
-sayGoodbye()
-
-SandwichMaker.placeOrder("rye", "capicola", "provolone")
