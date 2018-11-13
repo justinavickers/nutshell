95b2894428ba26963df15db26b646bff9dcf5f14
justinavickers <justinav38@gmail.com>
2018-11-13 13:03:44 -0600

    working on login email verification

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,12 +1,12 @@
-import newUserCreation from "./User_login/postUserFunction";
-import uponClickLogin from "./User_login/fetching";
+import registration from "./User_login/postUserFunction";
+import sessionStorageSetup from "./User_login/session_storage";
 import displayMessages from"./Messages/MessageGetFunction"
 import messageDisplay from "./Messages/MessageGetFunction";
+// import sessionStorageSetup from "./User_login/session_storage";
 // event listener for sign up and register buttons
 let existingUserButton = document.getElementById("loginButton")
 let newUsers = document.getElementById("signUpButton")
-newUsers.addEventListener("click", ()=> newUserCreation())
-existingUserButton.addEventListener("click", ()=>{ uponClickLogin()
+newUsers.addEventListener("click", ()=> registration.newUserCreation())
+existingUserButton.addEventListener("click", ()=>{ sessionStorageSetup()
 })
 messageDisplay()
-
