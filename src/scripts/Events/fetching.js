2a407145ea25a38988aa46f77758a4472f5c2e28
Max Milner <maxmilner31@yahoo.com>
2018-11-13 15:24:16 -0600

    html layout, events skeleton

---
+++ src/scripts/Events/fetching.js
@@ -0,0 +1,20 @@
+const eventAPIfunctions={
+  // Pulling events from the JSON database to the main JS
+  getEvent(){
+      fetch("http://localhost:8088/events")
+      .then(events => events.json())
+      .then(parsedEvents => parsedEvents)
+  },
+  // To post new events to the JSON database
+  postEvent(event){
+      console.log("postrunning", event)
+      return fetch("http://localhost:8088/events",{
+      method:"POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(event)
+  })}
+}
+
+export default eventAPIfunctions
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,11 +2,17 @@ import newUserCreation from "./User_login/postUserFunction";
 import uponClickLogin from "./User_login/fetching";
 import displayMessages from"./Messages/MessageGetFunction"
 import messageDisplay from "./Messages/MessageGetFunction";
-// event listener for sign up and register buttons
+import newEventCreation from "./Events/postEventFunction";
+
+// Event Listener for sign up and register buttons
 let existingUserButton = document.getElementById("loginButton")
 let newUsers = document.getElementById("signUpButton")
 newUsers.addEventListener("click", ()=> newUserCreation())
 existingUserButton.addEventListener("click", ()=>{ uponClickLogin()
+
+// Event Listener for Events
+let newEvents = document.getElementById("submitEvent")
+newEvents.addEventListener("click", () => newEventCreation())
 })
 messageDisplay()
 
