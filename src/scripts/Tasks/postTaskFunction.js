ca24b855f2ba6e1a818df7fe4777c7896e1c1755
wesseln1 <nwessel429@gmail.com>
2018-11-14 10:32:15 -0600

    pulling down master

--- src/scripts/Tasks/postTaskFunction.js
+++ src/scripts/Tasks/postTaskFunction.js
@@ -1,17 +1,19 @@
 import userAPIfunctions from "./task_fetching"
 import sessionStorageSetup from "../User_login/user_login"
     function newTaskCreation(){
+        // let user_id = sessionStorage.getItem("user_id", user_id)
         let taskName = document.querySelector("#taskName")
         let taskDate = document.querySelector("#tasDate")
         let taskCompleted = document.querySelector("#taskCompleted")
 
     let obj={
+        // user_id: user_id,
         taskName: "",
         taskDate: "",
         taskCompleted: ""
     }
     obj.taskName = (taskName.value)
-    obj.taskDate = (taskDate.value)
+    obj.taskDate = (taskDate)
     obj.taskCompleted = (taskCompleted.value)
     userAPIfunctions.postTask(obj)
     .then((response) =>response.json()
